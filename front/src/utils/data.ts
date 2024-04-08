e7bb4f671c18e84549b75784787e976441a844af
Nitay Elzur <elzur.nitay@gmail.com>
2024-04-08 13:30:43 +0300

    typo

--- front/src/utils/data.ts
+++ front/src/utils/data.ts
@@ -1,5 +1,5 @@
 // export const HOST = import.meta.env.DEV ? 'http://localhost:3000/data/' : import.meta.env.VITE_SERVER + '/data/'
-const HOST = import.meta.env.VITE_SERVER
+const HOST = import.meta.env.VITE_SERVER + 'data/'
 export const fetchAll = `${HOST}fetch-all`
 export const fetchEntry = `${HOST}fetch`
 export const send = `${HOST}send`
